86b09a706f49d96a13c9f60670b4ebbe0cac25c8
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-06-23 00:39:10 +0530

    add: admin selling prod - 1

---
+++ lib/features/admin/services/admin_services.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:amazon/constants/error_handling.dart';
+import 'package:amazon/constants/utils.dart';
+import 'package:amazon/models/product.dart';
+import 'package:cloudinary_public/cloudinary_public.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class AdminServices {
+  void sellProduct({
+    required BuildContext context,
+    required String name,
+    required String description,
+    required double price,
+    required double quantity,
+    required String category,
+    required List<File> images,
+  }) async {
+    try {
+      final cloudinary = CloudinaryPublic('facebboook', 'njv3qd0n');
+      List<String> imageUrls = [];
+
+      for (int i = 0; i < images.length; i++) {
+        CloudinaryResponse res = await cloudinary.uploadFile(
+          CloudinaryFile.fromFile(images[i].path, folder: name),
+        );
+        imageUrls.add(res.secureUrl);
+      }
+
+      Product product = Product(
+        name: name,
+        description: description,
+        quantity: quantity,
+        images: imageUrls,
+        category: category,
+        price: price,
+      );
+
+      
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+  }
+}
---
+++ lib/models/product.dart
@@ -0,0 +1,64 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class Product {
+  final String name;
+  final String description;
+  final double quantity;
+  final List<String> images;
+  final String category;
+  final double price;
+  final String? id;
+  final String? userId;
+  // final List<Rating>? rating;
+  Product({
+    // this.rating,
+    required this.name,
+    required this.description,
+    required this.quantity,
+    required this.images,
+    required this.category,
+    required this.price,
+    this.id,
+    this.userId,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'description': description,
+      'quantity': quantity,
+      'images': images,
+      'category': category,
+      'price': price,
+      'id': id,
+      'userId': userId
+      // 'rating': rating,
+    };
+  }
+
+  factory Product.fromMap(Map<String, dynamic> map) {
+    return Product(
+        name: map['name'] ?? '',
+        description: map['description'] ?? '',
+        quantity: map['quantity']?.toDouble() ?? 0.0,
+        images: List<String>.from(map['images']),
+        category: map['category'] ?? '',
+        price: map['price']?.toDouble() ?? 0.0,
+        id: map['_id'],
+        userId: map['userId']
+        // rating: map['ratings'] != null
+        //     ? List<Rating>.from(
+        //         map['ratings']?.map(
+        //           (x) => Rating.fromMap(x),
+        //         ),
+        //       )
+        //     : null,
+        );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Product.fromJson(String source) =>
+      Product.fromMap(json.decode(source));
+}
