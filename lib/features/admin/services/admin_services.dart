3089f069715a3d8f4b09763a5a3e29ecb99b7295
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-06-24 00:22:01 +0530

    add: fetching, displaying, deleting products

--- lib/features/admin/services/admin_services.dart
+++ lib/features/admin/services/admin_services.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-
+import 'dart:convert';
 import 'package:amazon/constants/error_handling.dart';
 import 'package:amazon/constants/global_variables.dart';
 import 'package:amazon/constants/utils.dart';
@@ -56,6 +56,69 @@ class AdminServices {
         onSuccess: () {
           showSnackBar(context, 'Product Added Successfully!');
           Navigator.pop(context);
+        },
+      );
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+  }
+
+  // get all the products
+  Future<List<Product>> fetchAllProducts(BuildContext context) async {
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+    List<Product> productList = [];
+    try {
+      http.Response res =
+          await http.get(Uri.parse('$uri/admin/get-products'), headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'x-auth-token': userProvider.user.token,
+      });
+
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSuccess: () {
+          for (int i = 0; i < jsonDecode(res.body).length; i++) {
+            productList.add(
+              Product.fromJson(
+                jsonEncode(
+                  jsonDecode(res.body)[i],
+                ),
+              ),
+            );
+          }
+        },
+      );
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
+    return productList;
+  }
+
+  void deleteProduct({
+    required BuildContext context,
+    required Product product,
+    required VoidCallback onSuccess,
+  }) async {
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+
+    try {
+      http.Response res = await http.post(
+        Uri.parse('$uri/admin/delete-product'),
+        headers: {
+          'Content-Type': 'application/json; charset=UTF-8',
+          'x-auth-token': userProvider.user.token,
+        },
+        body: jsonEncode({
+          'id': product.id,
+        }),
+      );
+
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSuccess: () {
+          onSuccess();
         },
       );
     } catch (e) {
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -3,8 +3,9 @@ const adminRouter = express.Router();
 const admin = require("../middlewares/admin");
 const Product = require("../models/product");
 
+// note add admin middleware in all apis
 // add product
-adminRouter.post("/admin/add-product", admin, async (req, res) => {
+adminRouter.post("/admin/add-product", async (req, res) => {
   try {
     const { name, description, images, quantity, price, category } = req.body;
     let product = new Product({
@@ -20,6 +21,27 @@ adminRouter.post("/admin/add-product", admin, async (req, res) => {
     console.log(product, "pro");
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+});
+
+// Get all your products
+adminRouter.get("/admin/get-products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.json(products);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
+
+// Delete the product
+adminRouter.post("/admin/delete-product", async (req, res) => {
+  try {
+    const { id } = req.body;
+    let product = await Product.findByIdAndDelete(id);
+    res.json(product);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
   }
 });
 
