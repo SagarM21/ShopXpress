a9658f406800403d8ac8cea17ec7a14b895be804
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-06-24 12:33:11 +0530

    add: display ratings done

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:convert';
 
+import 'package:amazon/models/rating.dart';
+
 class Product {
   final String name;
   final String description;
@@ -9,7 +11,7 @@ class Product {
   final String category;
   final double price;
   final String? id;
-  // final List<Rating>? rating;
+  final List<Rating>? rating;
   Product({
     // this.rating,
     required this.name,
@@ -19,6 +21,7 @@ class Product {
     required this.category,
     required this.price,
     this.id,
+    this.rating,
   });
 
   Map<String, dynamic> toMap() {
@@ -30,7 +33,7 @@ class Product {
       'category': category,
       'price': price,
       'id': id,
-      // 'rating': rating,
+      'rating': rating,
     };
   }
 
@@ -43,13 +46,13 @@ class Product {
       category: map['category'] ?? '',
       price: map['price']?.toDouble() ?? 0.0,
       id: map['_id'],
-      // rating: map['ratings'] != null
-      //     ? List<Rating>.from(
-      //         map['ratings']?.map(
-      //           (x) => Rating.fromMap(x),
-      //         ),
-      //       )
-      //     : null,
+      rating: map['ratings'] != null
+          ? List<Rating>.from(
+              map['ratings']?.map(
+                (x) => Rating.fromMap(x),
+              ),
+            )
+          : null,
     );
   }
 
